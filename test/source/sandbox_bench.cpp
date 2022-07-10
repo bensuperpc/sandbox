d5b7b8417245502b9df8e60e915f7fde4d38f462
Bensuperpc <bensuperpc@gmail.com>
2022-07-11 01:27:31 +0200

    Update code
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

---
+++ test/source/sandbox_bench.cpp
@@ -0,0 +1,33 @@
+#include <cstdint>  // uint32_t, uint64_t
+#include <random>  // std::mt19937
+#include <vector>  // std::vector
+
+#include <benchmark/benchmark.h>
+
+#include "vector/multi_array.hpp"  // benlib::multi_array
+#include "vector/utils.hpp"  // _KB, _MB, _GB, _TB
+
+static void multi_array_set_value_uint32_t_2D(benchmark::State& state)
+{
+  const auto width = static_cast<uint64_t>(state.range(0));
+  const auto height = static_cast<uint64_t>(state.range(0));
+  std::vector<uint64_t> v = {width, height};
+  auto grid = benlib::multi_array<uint32_t>(v);
+  grid.fill(0);
+  benchmark::DoNotOptimize(grid);
+
+  for (auto _ : state) {
+    for (uint64_t i = 0; i < width * height; ++i) {
+      grid.set_value(i, 42);
+    }
+    benchmark::ClobberMemory();
+  }
+
+  state.SetItemsProcessed(state.iterations() * width * height);
+  state.SetBytesProcessed(state.iterations() * width * height
+                          * sizeof(uint32_t));
+}
+BENCHMARK(multi_array_set_value_uint32_t_2D)
+    ->Name("multi_array_set_value_uint32_t_2D")
+    ->RangeMultiplier(8)
+    ->Range(8, 4_KB);
--- test/source/sandbox_test.cpp
+++ test/source/sandbox_test.cpp
@@ -6,3 +6,24 @@ auto main() -> int
 
   return lib.name == "sandbox" ? 0 : 1;
 }
+
+/*
+#include <cstdint>  // uint32_t, uint64_t
+#include <vector>  // std::vector
+
+#include "gtest/gtest.h"
+#include "vector/multi_array.hpp"
+
+TEST(multi_array, basic_1D_string)
+{
+  std::vector<uint64_t> v = {3};
+  auto grid = benlib::multi_array<std::string>(v);
+  grid.fill("");
+  grid.set_value(0, "a");
+  grid.set_value(1, "b");
+  grid.set_value(2, "c");
+  EXPECT_EQ(grid.get_value(0), "a");
+  EXPECT_EQ(grid.get_value(1), "b");
+  EXPECT_EQ(grid.get_value(2), "c");
+}
+*/
